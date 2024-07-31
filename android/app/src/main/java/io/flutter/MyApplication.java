9f758ed8a357db7e4c87468eefb422ca8618de25
Andreea Ciama <161943592+Andreea-Ciama@users.noreply.github.com>
2024-07-31 17:34:31 +0300

    Add files via upload

---
+++ android/app/src/main/java/io/flutter/MyApplication.java
@@ -0,0 +1,11 @@
+package com.example.my_flutter_app;
+
+import androidx.multidex.MultiDexApplication;
+
+public class MyApplication extends MultiDexApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Optional: Puteți adăuga aici alte inițializări necesare
+    }
+}
